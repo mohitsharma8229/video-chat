9a98f9a3d6d77c1456e084b40768f8e8246fc064
mohitsharma8229 <65356096+mohitsharma8229@users.noreply.github.com>
2023-07-28 20:29:56 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -17,7 +17,25 @@ navigator.mediaDevices.getUserMedia({
 .then((stream)=>{
     myStream=stream
     addVideoStream(myVideo,stream)
+    socket.on("user-connected",(userId)=>{
+        connectToNewUser(userId,stream)
+    })
+    peer.on("call",(call)=>{
+        call.answer(stream)
+        const video=document.createElement("video")
+        call.on("stream",(userVideoStream)=>{
+            addVideoStream(video,userVideoStream)
+        })
+    })
 })
+function connectToNewUser(userId,stream){
+    const call=peer.call(userId,stream)
+    const video=document.createElement("video")
+    call.on("stream",(userVideoStream)=>{
+        addVideoStream(video,userVideoStream)
+    })
+}
+
 function addVideoStream(video,stream){
     video.srcObject=stream
     video.addEventListener("loadedmetadata",()=>{
@@ -49,6 +67,56 @@ $(function () {
             $("#chat_message").val("");
         }
     })
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        }
+    })
+
+    $("#stop_video").click(function () {
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        }
+    })
+    $("#invite_button").click(function(){
+        const to=prompt("enter the email addres")
+        let data={
+            url:window.location.href,
+            to:to,
+        }
+        $.ajax({
+            url:"/send-mail",
+            type:"post",
+            data:JSON.stringify(data),
+            dataType:'json',
+            contentType:'application/json',
+            success:function(result){
+                alert("invite sent")
+            },
+            error:function(result){
+                console.log(result.responseJSON)
+            },
+        })
+    })
+
 })
 
 peer.on("open", (id) => {
--- server.js
+++ server.js
@@ -18,6 +18,16 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerServer);
+var nodemailer=require('nodemailer')
+const transporter=nodemailer.createTransport({
+    port:587,
+    host:"smtp.gmail.com",
+    auth:{
+        user:'mohitsharma.02929@gmail.com',
+        pass:'blngnhtdttnxjdjy'
+    },
+    secure:true,
+})
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -26,14 +36,31 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
+app.post("/send-mail",(req,res)=>{
+    const to=req.body.to
+    const url=req.body.url
+    const mailData={
+        from:"mohitsharma.02929@gamil.com",
+        to:to,
+        subject:"join the video chat with me",
+        html:`<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
+    }
+    transporter.sendMail(mailData,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({message:"invitation sent",message_id:info.messageId})
+    })
+})
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
+        io.to(roomId).emit("user-connected",userId)
         socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
     });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT||3030);
